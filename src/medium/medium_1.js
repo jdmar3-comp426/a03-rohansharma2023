6f0485802e865473ee4de657339a7f893bf57ac7
rohansharma2023 <rohansh@live.unc.edu>
2021-10-06 11:50:31 -0400

    medium_1 (getSum)

--- src/medium/medium_1.js
+++ src/medium/medium_1.js
@@ -8,7 +8,13 @@ import {variance} from "./data/stats_helpers.js";
  * prototype functions. Very useful
  */
 export function getSum(array) {
+    let sum = 0;
 
+    for(let i = 0; i < array.length; i++){
+        sum += array[i];
+    }
+
+    return sum;
 }
 
 
