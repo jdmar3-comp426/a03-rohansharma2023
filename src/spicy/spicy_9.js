0c74cb138ded72a5fa6d94cd442a259f79de5a12
rohansharma2023 <rohansh@live.unc.edu>
2021-11-02 20:54:21 -0400

    anevenisodd

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -175,7 +175,7 @@ export const filter = (arr, test) => {
  *   odd numbers. Use the "everyEven" function in this function.
  */
 export const allEvensAreOdd = (arr) => {
-
+  
 };
 
 
@@ -185,7 +185,7 @@ export const allEvensAreOdd = (arr) => {
  *   array is an odd number. Use the "someEven" function in this function.
  */
 export const anEvenIsOdd = (arr) => {
-
+  someEven(arr, x => (x % 2) === 1)
 };
 
 
