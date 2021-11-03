5020376910781dc81ff4685ce18d63e676882947
rohansharma2023 <rohansh@live.unc.edu>
2021-11-02 20:50:56 -0400

    someeven

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -134,7 +134,12 @@ export const everyEven = (arr, test) => {
  *    someEven([0, 0, 0, 0, 0], x => x === 0)  <--  returns true
  */
 export const someEven = (arr, test) => {
-
+  for(let i = 0; i < arr.length; i = i + 2){
+    if(test(arr[i]) == true){
+      return true;
+    }
+  }
+  return false;
 };
 
 
